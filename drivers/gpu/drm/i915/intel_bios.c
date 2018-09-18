projectacrn/acrn-kernel

drm/i915/icl: Find DSI presence for ICL
This patch detects DSI presence for ICL platform
by reading VBT. DSI detection is done while initializing
DSI using newly added function intel_gen11_dsi_init.

V2: Avoid behavior change of an existing function while finding
    DSI port (Jani N).
V3: Fix compile warning/error exposed by gcc 5.4.

Signed-off-by: Madhav Chauhan <madhav.chauhan@intel.com>

drivers/gpu/drm/i915/intel_bios.c
@@ -1839,6 +1839,7 @@ bool intel_bios_is_dsi_present(struct drm_i915_private *dev_priv,
 	const struct child_device_config *child;
 	u8 dvo_port;
 	int i;
+	bool ret = false;
 
 	for (i = 0; i < dev_priv->vbt.child_dev_num; i++) {
 		child = dev_priv->vbt.child_dev + i;
@@ -1850,19 +1851,37 @@ bool intel_bios_is_dsi_present(struct drm_i915_private *dev_priv,
 
 		switch (dvo_port) {
 		case DVO_PORT_MIPIA:
+			ret = true;
+			goto out;
 		case DVO_PORT_MIPIC:
-			if (port)
-				*port = dvo_port - DVO_PORT_MIPIA;
-			return true;
+			ret = IS_ICELAKE(dev_priv);
+			if (!ret) {
+				ret = true;
+				goto out;
+			}
+			break;
 		case DVO_PORT_MIPIB:
+			ret = IS_ICELAKE(dev_priv);
+			if (ret)
+				goto out;
+			break;
 		case DVO_PORT_MIPID:
-			DRM_DEBUG_KMS("VBT has unsupported DSI port %c\n",
-				      port_name(dvo_port - DVO_PORT_MIPIA));
+			DRM_ERROR("VBT has unsupported DSI port %c\n",
+				  port_name(dvo_port - DVO_PORT_MIPIA));
 			break;
 		}
 	}
 
-	return false;
+out:
+	if (ret) {
+		if (port)
+			*port = dvo_port - DVO_PORT_MIPIA;
+	} else {
+		DRM_ERROR("DSI info not present in VBT\n");
+	}
+
+
+	return ret;
 }
 
 /**

StanFox1984/drm-tip

drivers/gpu/drm/i915/intel_bios.c
@@ -2030,6 +2030,7 @@ bool intel_bios_is_dsi_present(struct drm_i915_private *dev_priv,
 	const struct child_device_config *child;
 	u8 dvo_port;
 	int i;
+	bool ret = false;
 
 	for (i = 0; i < dev_priv->vbt.child_dev_num; i++) {
 		child = dev_priv->vbt.child_dev + i;
@@ -2041,11 +2042,20 @@ bool intel_bios_is_dsi_present(struct drm_i915_private *dev_priv,
 
 		switch (dvo_port) {
 		case DVO_PORT_MIPIA:
+			ret = true;
+			goto out;
 		case DVO_PORT_MIPIC:
-			if (port)
-				*port = dvo_port - DVO_PORT_MIPIA;
-			return true;
+			ret = IS_ICELAKE(dev_priv);
+			if (!ret) {
+				ret = true;
+				goto out;
+			}
+			break;
 		case DVO_PORT_MIPIB:
+			ret = IS_ICELAKE(dev_priv);
+			if (ret)
+				goto out;
+			break;
 		case DVO_PORT_MIPID:
 			DRM_DEBUG_KMS("VBT has unsupported DSI port %c\n",
 				      port_name(dvo_port - DVO_PORT_MIPIA));
@@ -2053,7 +2063,16 @@ bool intel_bios_is_dsi_present(struct drm_i915_private *dev_priv,
 		}
 	}
 
-	return false;
+out:
+	if (ret) {
+		if (port)
+			*port = dvo_port - DVO_PORT_MIPIA;
+	} else {
+		DRM_DEBUG_KMS("DSI info not present in VBT\n");
+	}
+
+
+	return ret;
 }
 
 /**