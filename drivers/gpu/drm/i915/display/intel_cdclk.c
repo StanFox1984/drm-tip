5ecb497bd96bc8597c54e9b75351fdda81c3b788
Stanislav Lisovskiy <stanislav.lisovskiy@intel.com>
2020-03-10 15:59:55 +0200

    drm/i915: Adjust CDCLK accordingly to our DBuf bw needs
    According to BSpec max BW per slice is calculated using formula
    Max BW = CDCLK * 64. Currently when calculating min CDCLK we
    account only per plane requirements, however in order to avoid
    FIFO underruns we need to estimate accumulated BW consumed by
    all planes(ddb entries basically) residing on that particular
    DBuf slice. This will allow us to put CDCLK lower and save power
    when we don't need that much bandwidth or gain additional
    performance once plane consumption grows.
    
    v2: - Fix long line warning
        - Limited new DBuf bw checks to only gens >= 11
    
    v3: - Lets track used Dbuf bw per slice and per crtc in bw state
          (or may be in DBuf state in future), that way we don't need
          to have all crtcs in state and those only if we detect if
          are actually going to change cdclk, just same way as we
          do with other stuff, i.e intel_atomic_serialize_global_state
          and co. Just as per Ville's paradigm.
        - Made dbuf bw calculation procedure look nicer by introducing
          for_each_dbuf_slice_in_mask - we often will now need to iterate
          slices using mask.
        - According to experimental results CDCLK * 64 accounts for
          overall bandwidth across all dbufs, not per dbuf.
    
    v4: - Fixed missing const(Ville)
        - Removed spurious whitespaces(Ville)
        - Fixed local variable init(reduced scope where not needed)
        - Added some comments about data rate for planar formats
        - Changed struct intel_crtc_bw to intel_dbuf_bw
        - Moved dbuf bw calculation to intel_compute_min_cdclk(Ville)
    
    v5: - Removed unneeded macro
    
    v6: - Prevent too frequent CDCLK switching back and forth:
          Always switch to higher CDCLK when needed to prevent bandwidth
          issues, however don't switch to lower CDCLK earlier than once
          in 30 minutes in order to prevent constant modeset blinking.
          We could of course not switch back at all, however this is
          bad from power consumption point of view.
    
    v7: - Fixed to track cdclk using bw_state, modeset will be now
          triggered only when CDCLK change is really needed.
    
    Signed-off-by: Stanislav Lisovskiy <stanislav.lisovskiy@intel.com>

--- drivers/gpu/drm/i915/display/intel_cdclk.c
+++ drivers/gpu/drm/i915/display/intel_cdclk.c
@@ -21,10 +21,12 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
+#include <linux/time.h>
 #include "intel_atomic.h"
 #include "intel_cdclk.h"
 #include "intel_display_types.h"
 #include "intel_sideband.h"
+#include "intel_bw.h"
 
 /**
  * DOC: CDCLK / RAWCLK
@@ -2098,6 +2100,7 @@ static int intel_compute_min_cdclk(struct intel_cdclk_state *cdclk_state)
 	struct intel_crtc_state *crtc_state;
 	int min_cdclk, i;
 	enum pipe pipe;
+	struct intel_bw_state *bw_state;
 
 	for_each_new_intel_crtc_in_state(state, crtc, crtc_state, i) {
 		int ret;
@@ -2119,6 +2122,12 @@ static int intel_compute_min_cdclk(struct intel_cdclk_state *cdclk_state)
 	min_cdclk = cdclk_state->force_min_cdclk;
 	for_each_pipe(dev_priv, pipe)
 		min_cdclk = max(cdclk_state->min_cdclk[pipe], min_cdclk);
+
+	bw_state = intel_atomic_get_bw_state(state);
+	if (IS_ERR(bw_state))
+		return PTR_ERR(bw_state);
+
+	min_cdclk = max(bw_state->min_cdclk, min_cdclk);
 
 	return min_cdclk;
 }
